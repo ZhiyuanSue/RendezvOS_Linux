41a8c8e4f600374c76e19531310af2d7c45db8a0
ZhiyuanSue <2262387848@qq.com>
2025-06-27 21:29:54 +0800

    add syscall

--- include/syscall_entry.h
+++ include/syscall_entry.h
@@ -1,8 +1,9 @@
 #ifndef _SYSCALL_ENTRY_
 #define _SYSCALL_ENTRY_
 #include <common/types.h>
+#include <rendezvos/task/tcb.h>
 
 void init_syscall_entry();
-void syscall(i64 syscall_id, ...);
+void syscall(Arch_Syscall_Context* syscall_ctx);
 
 #endif
--- syscall_entry.c
+++ syscall_entry.c
@@ -1,12 +1,23 @@
 #include <modules/RendezvOS_Linux/syscall_entry.h>
 #include <modules/log/log.h>
 
-void syscall(i64 syscall_id, ...)
+void syscall(Arch_Syscall_Context* syscall_ctx)
 {
-        pr_info("Syscall with id %d\n", syscall_id);
+        pr_info("Syscall with id %d arg1: %d\n",
+                syscall_ctx->syscall_id,
+                syscall_ctx->arg1);
+        switch (syscall_ctx->syscall_id) {
+        case 60:
+                schedule(percpu(core_tm));
+                break;
+
+        default:
+                break;
+        }
         return;
 }
-static inline void set_syscall_entry(void (*syscall_entry)(i64 syscall_id, ...))
+static inline void
+set_syscall_entry(void (*syscall_entry)(Arch_Syscall_Context* syscall_ctx))
 {
         if (!syscall_entry) {
                 pr_error("[Error] no syscall entry is defined\n");
