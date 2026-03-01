c219eb5c45ebcae1eaf26139fc661da3fb9ba448
ZhiyuanSue <2262387848@qq.com>
2026-03-01 16:14:48 +0800

    add syscall id include

--- include/syscall_entry.h
+++ include/syscall_entry.h
@@ -4,6 +4,12 @@
 
 #include <rendezvos/trap.h>
 
+#ifdef _AARCH64_
+#include <modules/RendezvOS_Linux/arch/aarch64/syscall_ids.h>
+#elif defined _X86_64_
+#include <modules/RendezvOS_Linux/arch/x86_64/syscall_ids.h>
+#endif
+
 void init_syscall_entry();
 void syscall(struct trap_frame* syscall_ctx);
 
